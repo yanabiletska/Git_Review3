c8eb07657e4676f834a24bc8e086ca024636fcec
yanabiletska <61475421+yanabiletska@users.noreply.github.com>
2023-01-07 13:30:20 -0600

    first class and second class

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -1,4 +1,7 @@
 package first_package;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("First class");
+    }
 }
--- src/first_package/SecondClass.java
+++ src/first_package/SecondClass.java
@@ -1,4 +1,7 @@
 package first_package;
 
 public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Second class");
+    }
 }
