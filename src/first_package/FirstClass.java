b2e065dd902d00f8fb166d9d1b11b2fa66d7bf63
yanabiletska <61475421+yanabiletska@users.noreply.github.com>
2023-01-07 13:31:30 -0600

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -3,5 +3,8 @@ package first_package;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First class");
+        //some changes were made
+        System.out.println("First class");
+        
     }
 }
